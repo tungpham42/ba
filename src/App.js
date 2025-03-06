d09734148fea0ffde2f138ec56f536ceed433e73
Tung Pham <tung.42@gmail.com>
2025-03-06 14:59:48 +0700

    Footer.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import LessonPage from "./pages/LessonPage";
+import Footer from "./components/Footer";
 
 const App = () => {
   return (
@@ -9,6 +10,7 @@ const App = () => {
         <Route path="/" element={<Home />} />
         <Route path="/lesson/:id" element={<LessonPage />} />
       </Routes>
+      <Footer />
     </Router>
   );
 };
