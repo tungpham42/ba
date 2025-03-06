e9809035b750e6a033b3e3597db3e2e8c2dc339d
Tung Pham <tung.42@gmail.com>
2025-03-07 05:41:18 +0700

    Update LessonCard.js

--- src/components/LessonCard.js
+++ src/components/LessonCard.js
@@ -7,16 +7,8 @@ const LessonCard = ({ lesson: { id, title, content }, onStart, completed }) => {
     <Col key={id} xxl={4} xl={4} lg={6} md={6} sm={12} className="mb-4">
       <Card className="d-flex flex-column h-100 shadow-lg">
         <Card.Body className="d-flex flex-column">
-          {/* Title with Completed Indicator */}
-          <Card.Title className="d-flex justify-content-between align-items-center">
-            {`${id}. ${title}`}
-            {completed && (
-              <Badge bg="success">
-                <FontAwesomeIcon icon={faCheckCircle} className="me-1" />
-                Completed
-              </Badge>
-            )}
-          </Card.Title>
+          {/* Title */}
+          <Card.Title>{`${id}. ${title}`}</Card.Title>
 
           <Card.Text
             dangerouslySetInnerHTML={{
@@ -24,12 +16,18 @@ const LessonCard = ({ lesson: { id, title, content }, onStart, completed }) => {
             }}
           />
 
-          {/* Start Lesson Button */}
-          <div className="mt-auto d-flex justify-content-start gap-3">
+          {/* Button and Completed Indicator */}
+          <div className="mt-auto d-flex justify-content-between align-items-center">
             <Button variant="primary" onClick={() => onStart(id)}>
               <FontAwesomeIcon icon={faPlayCircle} className="me-2" />
               Start Lesson
             </Button>
+            {completed && (
+              <Badge bg="success" className="p-2 text-uppercase">
+                <FontAwesomeIcon icon={faCheckCircle} className="me-1" />
+                Completed
+              </Badge>
+            )}
           </div>
         </Card.Body>
       </Card>
